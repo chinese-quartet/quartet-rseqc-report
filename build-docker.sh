f53b5eb35a3f6d1626390e7740c34b7b79a04b1b
yjcyxky <yjcyxky@163.com>
2023-02-09 22:07:23 +0800

    Add a docker image as a runner.

--- build-docker.sh
+++ build-docker.sh
@@ -7,7 +7,12 @@ VERSION=$(git describe --tags `git rev-list --tags --max-count=1` --always)
 # dynamically pull more interesting stuff from latest git commit
 HASH=$(git show-ref --head --hash=8 head)  # first 8 letters of hash should be enough; that's what GitHub uses
 
-# Build base docker image
+# Build standalone docker image
 docker build -t quartet-rseqc-report:${VERSION}-${HASH} . && \
 docker tag quartet-rseqc-report:${VERSION}-${HASH} ghcr.io/chinese-quartet/quartet-rseqc-report:${VERSION}-${HASH} && \
 docker push ghcr.io/chinese-quartet/quartet-rseqc-report:${VERSION}-${HASH}
+
+# Build runner docker image
+docker build -f Dockerfile.runner -t quartet-rseqc-report:runner-${VERSION}-${HASH} . && \
+docker tag quartet-rseqc-report:runner-${VERSION}-${HASH} ghcr.io/chinese-quartet/quartet-rseqc-report:runner-${VERSION}-${HASH} && \
+docker push ghcr.io/chinese-quartet/quartet-rseqc-report:runner-${VERSION}-${HASH}
