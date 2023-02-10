3e42ed6c0daa9384eb8fa0909de765e880e80092
yjcyxky <yjcyxky@163.com>
2023-02-10 13:13:15 +0800

    Add action for runner.

--- build-docker.sh
+++ build-docker.sh
@@ -11,8 +11,3 @@ HASH=$(git show-ref --head --hash=8 head)  # first 8 letters of hash should be e
 docker build -t quartet-rseqc-report:${VERSION}-${HASH} . && \
 docker tag quartet-rseqc-report:${VERSION}-${HASH} ghcr.io/chinese-quartet/quartet-rseqc-report:${VERSION}-${HASH} && \
 docker push ghcr.io/chinese-quartet/quartet-rseqc-report:${VERSION}-${HASH}
-
-# Build runner docker image
-docker build -f Dockerfile.runner -t quartet-rseqc-report:runner-${VERSION}-${HASH} . && \
-docker tag quartet-rseqc-report:runner-${VERSION}-${HASH} ghcr.io/chinese-quartet/quartet-rseqc-report:runner-${VERSION}-${HASH} && \
-docker push ghcr.io/chinese-quartet/quartet-rseqc-report:runner-${VERSION}-${HASH}
