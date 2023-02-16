8c7167819cd69da852c82b7bba3517258468cabb
yjcyxky <yjcyxky@163.com>
2023-02-16 19:30:21 +0800

    Improve error messages.

--- bin/rseqc.py
+++ bin/rseqc.py
@@ -26,22 +26,22 @@ def rseqc():
 
 @rseqc.command(help="Run the pipeline for RNA-Seq data.")
 @click.option('--r1', required=True,
-              type=click.Path(exists=True, file_okay=True),
+              type=click.Path(exists=True, file_okay=True, dir_okay=False),
               help="The fastq file with suffixes of _R1.fastq.gz or _R1.fq.gz.")
 @click.option('--r2', required=True,
-              type=click.Path(exists=True, file_okay=True),
+              type=click.Path(exists=True, file_okay=True, dir_okay=False),
               help="The fastq file with suffixes of _R2.fastq.gz or _R2.fq.gz.")
 @click.option('--hisat2-index', '-i', required=True,
-              type=click.Path(exists=True, dir_okay=True),
+              type=click.Path(exists=True, dir_okay=True, file_okay=False),
               help="The index for the reference genome.")
 @click.option('--gtf', '-g', required=True,
-              type=click.Path(exists=True, file_okay=True),
+              type=click.Path(exists=True, file_okay=True, dir_okay=False),
               help="The gtf file.")
 @click.option('--output-dir', required=False,
-              type=click.Path(exists=True, dir_okay=True),
+              type=click.Path(exists=True, dir_okay=True, file_okay=False),
               help="The output directory.")
 @click.option('--fastq-screen-conf', '-s', required=True,
-              type=click.Path(exists=True, file_okay=True),
+              type=click.Path(exists=True, file_okay=True, file_okay=False),
               help="The config file for fastq-screen, the reference genomes must be located in the same directory with config file.")
 def workflow(r1, r2, hisat2_index, fastq_screen_conf, gtf, output_dir):
     if not re.match(r'.*_R1.(fastq|fq).gz', r1):
@@ -55,6 +55,12 @@ def workflow(r1, r2, hisat2_index, fastq_screen_conf, gtf, output_dir):
     wdl_dir = '/venv/workflow'
     if not os.path.exists(wdl_dir):
         print("Cannot find the workflow, please contact the administrator.")
+
+    for item in ["GRCh38.d1.vd1.fa.1.ht2", "GRCh38.d1.vd1.fa.2.ht2", "GRCh38.d1.vd1.fa.3.ht2",
+              "GRCh38.d1.vd1.fa.4.ht2", "GRCh38.d1.vd1.fa.5.ht2", "GRCh38.d1.vd1.fa.6.ht2",
+              "GRCh38.d1.vd1.fa.7.ht2", "GRCh38.d1.vd1.fa.8.ht2"]:
+        if not os.path.exists(os.path.join(hisat2_index, item)):
+            raise Exception("Cannot find %s in %s, you need to download hisat2 index files." % (item, hisat2_index))
 
     project_name = "rseqc"
     data_dict = {
